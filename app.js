4ff8bca041d737eef6c5dbe774d82de3bd9485f8
farukeynalli <afarukeynalli@gmail.com>
2024-04-08 19:35:53 +0300

    lesson 02

--- app.js
+++ app.js
@@ -2,12 +2,26 @@ const express = require("express");
 
 const app = express();
 
-const port = 3000;
+// Template Engine
+app.set("view engine", "ejs");
 
+//MiddleWares
+app.use(express.static("public"));
+
+//Routes
 app.get("/", (req, res) => {
-  res.status(200).send("indexxxxx");
+  res.status(200).render("index", {
+    page_name: "index",
+  });
 });
 
+app.get("/about", (req, res) => {
+  res.status(200).render("about", {
+    page_name: "about",
+  });
+});
+
+const port = 3000;
 app.listen(port, () => {
   console.log(`App listen on port ${port}`);
 });
